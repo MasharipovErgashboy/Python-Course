1f8b9d7e2d468af740c1e262955bcda12ad5cabb
Ergashboy <fullstackdeveloper223341@gmail.com>
2025-04-08 16:48:37 +0500

    2-vazifa bajarildi

--- Task1/vazifa1.py
+++ Task1/vazifa1.py
@@ -1,3 +1,31 @@
-a = int(input("sonni kiriting:"))#inputga kiritilgan stringni initga o'giradi
-p = 4 * a # perimetr 4 ta tomon yigindisiga teng
-print(p)
+# a = int(input("sonni kiriting:"))#inputga kiritilgan stringni initga o'giradi
+# p = 4 * a # perimetr 4 ta tomon yigindisiga teng
+# print(p)
+
+# begin - 3
+# To'g'ri to'rtburchakning tomonlari a va b berilgan. Uning yuzasi S = a* b; va P = 2* (a + b) perimetri aniqlansin.
+
+# a = float(input("a tomon uzunligini kiriting: "))
+# b = float(input("b tomon uzunligini kiriting: "))
+#
+# S = a * b
+# P = 2 * (a + b)
+#
+# print("Yuza (S):", S)
+# print("Perimetr (P):", P)
+
+
+# begin - 4
+# Nolga teng bo'lmagan ikkita son berilgan. Ularning yig'indisini, ko'paytmasini va har birining kvadrati aniqlansin.
+# x = float(input("1-sonni kiriting (nol bo'lmasin): "))
+# y = float(input("2-sonni kiriting (nol bo'lmasin): "))
+#
+# summa = x + y
+# kopaytma = x * y
+# x_kvadrat = x ** 2
+# y_kvadrat = y ** 2
+#
+# print("Yig'indisi:", summa)
+# print("Ko'paytmasi:", kopaytma)
+# print("Birinchi sonning kvadrati:", x_kvadrat)
+# print("Ikkinchi sonning kvadrati:", y_kvadrat)
