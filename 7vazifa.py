ff84150b76ef8971ac5f9da075049dcd41e41b10
Ergashboy <fullstackdeveloper223341@gmail.com>
2025-05-22 13:18:47 +0500

    yangi vazivalar joylandi

--- 7vazifa.py
+++ 7vazifa.py
@@ -1,26 +1,45 @@
-matn = "dasturlash juda yaxshi dasturlash"
-sozlar = matn.split()
-lugat = {}
-for soz in sozlar:
-    if soz in lugat:  
-        lugat[soz] += 1  
-    else:  
-        lugat[soz] = 1  
-print(lugat)
+# matn = "dasturlash juda yaxshi dasturlash"
+# sozlar = matn.split()
+# lugat = {}
+# for soz in sozlar:
+#     if soz in lugat:  
+#         lugat[soz] += 1  
+#     else:  
+#         lugat[soz] = 1  
+# print(lugat)
 
 
 
-foydalanuvchi = {
-    "ism": "Ali",
-    "yosh": 20,
-    "shahar": "Andijon"
-    }
-foydalanuvchi["yosh"] = 21
-foydalanuvchi["shahar"] = "Toshkent"
-del foydalanuvchi["ism"]
-print(foydalanuvchi)
+# foydalanuvchi = {
+#     "ism": "Ali",
+#     "yosh": 20,
+#     "shahar": "Andijon"
+#     }
+# foydalanuvchi["yosh"] = 21
+# foydalanuvchi["shahar"] = "Toshkent"
+# del foydalanuvchi["ism"]
+# print(foydalanuvchi)
 
 
-sonlar = [1, 2, 2, 3, 4, 4, 5]
-noyob_sonlar = set(sonlar)
-print(noyob_sonlar)
+# sonlar = [1, 2, 2, 3, 4, 4, 5]
+# noyob_sonlar = set(sonlar)
+# print(noyob_sonlar)
+
+
+# students = [
+#     {"ism": "Ali", "yosh": 20, "shahar": "Andijon"},
+#     {"ism": "Vali", "yosh": 22, "shahar": "Toshkent"},
+#     {"ism": "Hasan", "yosh": 19, "shahar": "Samarqand"},
+#     {"ism": "Husan", "yosh": 21, "shahar": "Buxoro"}
+# ]
+# for student in students:
+#     if student["yosh"] > 20:
+#         print(f"{student['ism']} {student['yosh']} yoshda, {student['shahar']} shahrida yashaydi.")
+#     else:
+#         print(f"{student['ism']} {student['yosh']} yoshda, {student['shahar']} shahrida yashaydi.")
+
+# numbers = {1, 2, 3, 4, 5,5, 6, 7, 8, 9}
+# print(type(numbers))
+# print(numbers)
+# print[1] = 50
+# print(numbers)
